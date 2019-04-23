d925a659777904f58cf6df3502cc88772dcdf53d
gabii6431 <gabii6431@gmail.com>
2019-04-23 08:55:15 -0300

    Devenvolvimento das telas iniciais

---
+++ Digitando/app/src/main/java/com/example/digitando/Palavras_Faceis.java
@@ -0,0 +1,13 @@
+package com.example.digitando;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Palavras_Faceis extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_palavras__faceis);
+    }
+}
