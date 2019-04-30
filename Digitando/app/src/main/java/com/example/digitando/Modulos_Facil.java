c24d02decdfd902d748f96c2d20e6a114876c2b3
gabii6431 <gabii6431@gmail.com>
2019-04-30 12:11:37 -0300

    Mover o menu superior

--- Digitando/app/src/main/java/com/example/digitando/Modulos_Facil.java
+++ Digitando/app/src/main/java/com/example/digitando/Modulos_Facil.java
@@ -1,12 +1,13 @@
 package com.example.digitando;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class Modulos_Facil extends AppCompatActivity implements View.OnClickListener {
+public class Modulos_Facil extends Activity implements View.OnClickListener {
     private Button btnJogar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
