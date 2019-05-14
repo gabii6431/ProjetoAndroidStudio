5680b5177b09812fa2e2260e3c89c85e6f8b149a
Laís <lais.magalhaes13@gmail.com>
2019-05-14 12:12:13 -0300

    add shared progressBar

--- Digitando/app/src/main/java/com/example/digitando/Modulos_Facil.java
+++ Digitando/app/src/main/java/com/example/digitando/Modulos_Facil.java
@@ -2,20 +2,37 @@ package com.example.digitando;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 
 public class Modulos_Facil extends Activity implements View.OnClickListener {
     private Button btnJogar;
+    private ProgressBar progressModulo1;
+    private SharedPreferences preferencias;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        preferencias = getSharedPreferences("prefModulo",0);
+        String verifica = preferencias.getString("verifica", null);
+
+        if(verifica == "true"){
+            progressModulo1.setProgress(progressModulo1.getProgress()+1);
+
+        }
+
         setContentView(R.layout.activity_modulos__facil);
 
         btnJogar = (Button) findViewById(R.id.btnJogar2);
         btnJogar.setOnClickListener(this);
+
+
+        progressModulo1 = (ProgressBar) findViewById(R.id.progressModulo1);
+
+
     }
 
     @Override
--- Digitando/app/src/main/java/com/example/digitando/Palavras_Faceis.java
+++ Digitando/app/src/main/java/com/example/digitando/Palavras_Faceis.java
@@ -2,6 +2,7 @@ package com.example.digitando;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,19 +10,29 @@ import android.widget.Button;
 
 public class Palavras_Faceis extends Activity implements View.OnClickListener {
     private Button btnEnviar;
+    private SharedPreferences preferenciasModulo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_palavras__faceis);
-
         btnEnviar = (Button) findViewById(R.id.btnEnviar);
         btnEnviar.setOnClickListener(this);
+
+        preferenciasModulo = getSharedPreferences("prefModulo",0);
     }
 
     @Override
     public void onClick(View v) {
         if(v == btnEnviar){
+            SharedPreferences.Editor escritor = preferenciasModulo.edit();
             //Mudar aqui
+            //verificar se palavra está certa
+            if(true){
+                escritor.putString("verifica","true");
+            }else{
+                escritor.putString("verifica","false");
+            }
+            escritor.commit();
 
             Intent i = new Intent(this, Tela_acertou.class);
             startActivity(i);
