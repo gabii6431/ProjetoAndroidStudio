3d07706cc17382839a3620a2819b900839af0a55
Laís <lais.magalhaes13@gmail.com>
2019-05-28 12:27:20 -0300

    tratamento do progress bar no modulo facil

--- Digitando/app/src/main/java/com/example/digitando/Modulos_Facil.java
+++ Digitando/app/src/main/java/com/example/digitando/Modulos_Facil.java
@@ -11,27 +11,37 @@ import android.widget.ProgressBar;
 
 public class Modulos_Facil extends Activity implements View.OnClickListener {
     private Button btnJogar;
-    private ProgressBar progressModulo1;
+    private ProgressBar progressModulo1, progressModulo2, progressModulo3;
     private SharedPreferences preferencias;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modulos__facil);
         preferencias = getSharedPreferences("prefModulo",0);
         String verifica = preferencias.getString("verifica", null);
-
-        if(verifica == "true"){
-            progressModulo1.setProgress(progressModulo1.getProgress()+1);
-
-        }
-
-        setContentView(R.layout.activity_modulos__facil);
 
         btnJogar = (Button) findViewById(R.id.btnJogar2);
         btnJogar.setOnClickListener(this);
 
-
         progressModulo1 = (ProgressBar) findViewById(R.id.progressModulo1);
+        progressModulo2 = (ProgressBar) findViewById(R.id.progressModulo2);
+        progressModulo3 = (ProgressBar) findViewById(R.id.progressModulo3);
 
+        if(verifica == "true"){
+            if(progressModulo1.getProgress() == 10){
+                if(progressModulo2.getProgress() != 10){
+                    progressModulo2.setProgress(progressModulo2.getProgress()+1);
+                }else if(progressModulo2.getProgress()  == 10){
+                    if(progressModulo3.getProgress() != 10){
+                        progressModulo3.setProgress(progressModulo3.getProgress()+1);
+                    }else if(progressModulo3.getProgress() == 10){
+                        //mandar um shared preference que passou para o nivel 2
+                    }
+                }
+            }else if(progressModulo1.getProgress() != 10){
+                progressModulo1.setProgress(progressModulo1.getProgress()+1);
+            }
+        }
 
     }
 
--- Digitando/app/src/main/java/com/example/digitando/ProximoModulo.java
+++ Digitando/app/src/main/java/com/example/digitando/ProximoModulo.java
@@ -1,14 +1,28 @@
 package com.example.digitando;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class ProximoModulo extends Activity {
+public class ProximoModulo extends Activity implements View.OnClickListener {
+    private Button btnProximoModulo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_proximo_modulo);
+
+        btnProximoModulo = (Button) findViewById(R.id.btnProximoModulo);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == btnProximoModulo){
+            //receber a shared preference para poder ver em qual nivel está
+            //ver qual janela de nivel vai abrir
+        }
     }
 }
