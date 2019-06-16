e60b14bd24c925155b230a3a6e397f52917fc638
gabii6431 <gabii6431@gmail.com>
2019-06-15 21:38:13 -0300

    Add modulo médio (em andamento)

---
+++ Digitando/app/src/main/java/com/example/digitando/Modulos_Medio.java
@@ -0,0 +1,13 @@
+package com.example.digitando;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class modulos_medio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modulos_medio);
+    }
+}
